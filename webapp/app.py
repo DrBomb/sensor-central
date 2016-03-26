4f9fe5aac881c600224e2e7cc416e1b57c0af503
Juan Molero <juanmolerog@gmail.com>
2016-03-26 00:55:48 -0430

    WE'RE MAKING PROGRESS MOM

--- webapp/app.py
+++ webapp/app.py
@@ -1,32 +1,40 @@
 from flask import Flask, render_template, url_for, redirect
-from database import db
-from views import dht
+from database.database import db
+from dht import dht
 
-def dreate_app():
+def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+    app.config['SQLALCHEMY_ECHO'] = True
     db.init_app(app)
     return app
 
-dht1 = dht({
-    "name":"DHT1",
-    "title":"DHT11",
-    "mqtt_feeds":[
-        "/dht1/temperature",
-        "/dht1/humidity"
-        ]
-    })
-    
-app.register_blueprint(dht1.view)
-
-@app.route('/')
-def index2():
-  return render_template('index.html',title = config['app']['title'])
-
-@app.route('/index')
-def index():
-  return redirect(url_for('/'))
-  
-
 if __name__ == "__main__":
+  app = create_app()
+  dht1 = dht({
+      "name":"DHT1",
+      "title":"DHT11",
+      "mqtt_feeds":[
+          "/dht1/temperature",
+          "/dht1/humidity"
+          ]
+      })
+  dht2 = dht({
+      "name":"DHT2",
+      "title":"DHT2",
+      "mqtt_feeds":[
+          "/dht2/temperature",
+          "/dht2/humidity"
+          ]
+      })
+  app.register_blueprint(dht1.view, url_prefix='/'+dht1.name)
+  app.register_blueprint(dht2.view, url_prefix='/'+dht2.name)
+  with app.app_context():
+      db.create_all()
+  @app.route('/')
+  def index2():
+      return render_template('index.html',title = config['app']['title'])
+  @app.route('/index')
+  def index():
+      return redirect(url_for('/'))
   app.run(debug=True)
--- webapp/dht.py
+++ webapp/dht.py
@@ -2,20 +2,25 @@ from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
 from datetime import datetime
 from json import dumps
-from .database import db
+from new import classobj
+from database.database import db
+
+
 class dht:
     def __init__(self,params):
-        self.view = Blueprint(params['name'],__name__,template_folder="templates")
+        self.view = Blueprint(params['name'],__name__,template_folder='templates')
         self.title = params['title']
         self.mqtt_feeds = params['mqtt_feeds']
         self.name = params['name']
-        self.table = class (db.Model):
-            id = db.Column(db.Integer,primary_key=True)
-            data_type = db.Column(db.Enum(("hum","temp")))
-            timestamp = db.Column(db.DateTime)
+        self.table = classobj(self.name,(db.Model,),{
+            "__tablename__": self.name,
+            "id" : db.Column(db.Integer,primary_key=True),
+            "data_type" : db.Column(db.Enum("hum","temp")),
+            "timestamp" : db.Column(db.DateTime)
+            })
         @self.view.route('/')
         def index(): 
-            return render_template('dht_both.html',params=self.params)
+            return render_template('dht.html',params={"title":self.name})
         @self.view.route('/temperature/<int:count>')
         def temperature(count):
             session = self.Session()
