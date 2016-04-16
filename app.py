10788425bc70b88f519ba2f85620061d694436be
Juan Molero <juanmolerog@gmail.com>
2016-04-15 19:59:09 -0430

    Added mqtt lock

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, url_for, redirect
 from threading import Thread
 import mqtt
+from zc import lockfile
 from database.database import db
 from dht import dht
 from simple import Simple
+
 
 def create_app():
     app = Flask(__name__)
@@ -12,39 +14,51 @@ def create_app():
     db.init_app(app)
     return app
 
+
 app = create_app()
+
 views = [dht({
-    "name":"DHT1",
-    "title":"DHT11",
-    "mqtt_feeds":[
+    "name": "DHT1",
+    "title": "DHT11",
+    "mqtt_feeds": [
         "/dht1/temperature",
         "/dht1/humidity"
-        ],
-    "app":app
-    }),
+    ],
+    "app": app
+}),
     Simple({
-    "name":"Simple",
-    "title":"Simple",
-    "mqtt_feeds":[
-        "/simple/counter"
+        "name": "Simple",
+        "title": "Simple",
+        "mqtt_feeds": [
+            "/simple/counter"
         ],
-    "app":app
+        "app": app
     })]
 for x in views:
-    app.register_blueprint(x.view, url_prefix='/'+x.name)
+    app.register_blueprint(x.view, url_prefix='/' + x.name)
 with app.app_context():
     db.create_all()
-mqtt.views = views
-mqtt.mqtt.connect("localhost")
-worker = Thread(target=mqtt.mqtt.loop_forever)
-worker.daemon = True
-worker.start()
-app.background_thread = worker
+
+try:
+    lock = lockfile.LockFile('lock')
+    mqtt.views = views
+    mqtt.mqtt.connect("localhost")
+    worker = Thread(target=mqtt.mqtt.loop_forever)
+    worker.daemon = True
+    worker.start()
+except lockfile.LockError:
+    print("Can't lock file")
+
+
 @app.route('/')
 def index2():
-    return render_template('index.html',title = config['app']['title'])
+    return render_template('index.html', title="Sensor Central")
+
+
 @app.route('/index')
 def index():
     return redirect(url_for('/'))
+
+
 if __name__ == "__main__":
     app.run(debug=False)
